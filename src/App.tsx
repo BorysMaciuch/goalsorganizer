a87d1cbdde434b283edf6a93c0b92e93d8cc6604
BorysMaciuch <borysmaciuch@gmail.com>
2020-10-25 15:12:48 +0100

    Add Home, ErrorPage pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
-import Home from "./home/script";
+import Home from "./pages/Home/script";
+import ErrorPage from './pages/ErrorPage/script'
 
 const App = () => {
   return (
+    <>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route component={ErrorPage}/>
     </Switch>
+    </>
   );
 };
 
---
+++ src/pages/ErrorPage/script.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const ErrorPage: React.FC = () => {
+    return (
+        <div>404 Nothing here yet</div>
+    )
+}
+
+export default ErrorPage
