dc0d70236cb2c662c8d3ab2f498f61ae7b734fcf
BorysMaciuch <borysmaciuch@gmail.com>
2020-10-25 15:19:23 +0100

    Add GoalsList page and NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/script";
 import ErrorPage from './pages/ErrorPage/script'
+import GoalsList from './pages/GoalsList/script'
+import NavBar from './components/NavBar/script'
 
 const App = () => {
   return (
     <>
+    <NavBar/>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route component={ErrorPage}/>
+      <Route path="/goalslist" component={GoalsList}/>
     </Switch>
     </>
   );
---
+++ src/components/NavBar/script.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar: React.FC = () => {
+  return <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/goalslist'>Goals List</Link>
+  </nav>;
+};
+
+export default NavBar;
