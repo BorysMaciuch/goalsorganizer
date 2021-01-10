e4ac44628a511cd19c3da64757c632ec6303b5a5
BorysMaciuch <borysmaciuch@gmail.com>
2021-01-10 20:01:34 +0100

    Change styling of Container component

--- src/components/Container/styled.ts
+++ src/components/Container/styled.ts
@@ -1,13 +1,25 @@
 import styled from 'styled-components'
-
-export const Container = styled.div`
+interface ContainerProps {
+    row?: boolean;
+    shadow?: string;
+    bgColor?: string;
+}
+export const Container = styled.div<ContainerProps>`
     display: flex;
-    flex-direction: column;
+    flex-direction: ${({row}) => row ? 'row' : 'column'};
     justify-content: center;
     align-items: center;
     width: 80%;
-    margin: auto;
+    margin: 10px auto;
+    padding: 12px;
     border-radius: ${({ theme }) => theme.radius.normal};
-    background-color: ${({ theme }) => theme.white};
+    background-color: ${({ bgColor }) => bgColor ? bgColor : 'none'};
+    box-shadow: ${({ shadow }) => shadow ? shadow : 'none'};
+    @media (max-width: 1500px) {
+        flex-direction: column;
+    }
     
+`
+export const RelativeContainer = styled.div`
+    position: relative;
 `
