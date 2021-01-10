53e37c93981d7a4f0bac3e4394c3ead478d88cbe
BorysMaciuch <borysmaciuch@gmail.com>
2021-01-10 19:59:08 +0100

    Add font 'Lato', add additional styles to theme object

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,22 +1,31 @@
 import { createGlobalStyle } from "styled-components";
 
 export const theme = {
+  colors: {
+    main: "#1d3557",
+    lightBlue: "#f1faee",
+    delete: '#e63946',
+    edit: '#e9c46a',
+    white: '#ffffff'
+  },
 
-    bg: "#1d3557",
-    white: '#f1faee',
-    radius: {
-        normal: '10px   '
-    }
+  radius: {
+    normal: "6px",
+    button: "12px",
+  },
+  shadow: {
+    normal: "0 3px 1px -2px rgba(0,0,0,.2)"
   }
+};
 
 export const GlobalStyles = createGlobalStyle`
 * {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;    
+    box-sizing: border-box;
+    font-family: 'Lato', sans-serif;    
 }
 body {
-    background-color: ${theme.bg}
+    background-color: ${theme.colors.main}
 }
 `;
-
