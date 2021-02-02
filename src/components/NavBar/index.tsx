80046e8a12e60cbdaecca1ef25929ad6864ce17d
BorysMaciuch <borysmaciuch@gmail.com>
2021-02-02 20:21:12 +0100

    Add auth0 basic setup

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LoginButton from '../LoginButton/index'
+import LogoutButton from '../LogoutButton/index'
+import { NavBarStyled } from './styled'
 
 const NavBar: React.FC = () => {
-  return <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/goalslist'>Goals List</Link>
-  </nav>;
+  return <NavBarStyled>
+    <Link to='/'>Home</Link>
+    <Link to='/goalslist'>Goals List</Link>
+    <LoginButton />
+    <LogoutButton />
+  </NavBarStyled>;
 };
 
 export default NavBar;
