87f0d007c8227417a010dd7d0423c1fb7887aca7
BorysMaciuch <borysmaciuch@gmail.com>
2021-02-04 20:50:23 +0100

    Further refactor to context API

--- src/components/ActionPoint/index.tsx
+++ src/components/ActionPoint/index.tsx
@@ -4,24 +4,18 @@ import { DeleteButton, EditButton } from "../Button/styled";
 import { Container } from "../Container/styled";
 import { deleteActionPoint } from "../../services/api/index";
 import GoalsContext from "../../services/context/GoalsContext";
+import GoalContext from "../../services/context/GoalContext";
 
 export interface ActionPointType {
   description: string;
   id: string;
-  goalId: string;
 }
-export const ActionPoint: React.FC<ActionPointType> = ({
-  description,
-  id,
-  goalId,
-}) => {
-  const { handleSetActiveActionPoint } = useContext(
-    GoalsContext
-  );
+export const ActionPoint: React.FC<ActionPointType> = ({ description, id }) => {
+  const { handleSetActiveActionPoint } = useContext(GoalsContext);
+  const { goalId } = useContext(GoalContext);
   return (
     <Container row key={uuidv4()}>
       {description}
-      {id}
       <EditButton onClick={() => handleSetActiveActionPoint(goalId, id)}>
         Edit action point
       </EditButton>
--- src/components/GoalsList/index.tsx
+++ src/components/GoalsList/index.tsx
@@ -2,23 +2,26 @@ import React, { useContext, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Goal } from "../Goal";
 import GoalsContext from "../../services/context/GoalsContext";
+import GoalContext from "../../services/context/GoalContext";
 
 const GoalsList: React.FC = () => {
-  const { goals, handleGetGoals } = useContext(
-    GoalsContext
-  );
+  const { goals, handleGetGoals } = useContext(GoalsContext);
   useEffect(() => {
     handleGetGoals();
   }, []);
   return (
     <>
       {goals.map((goal) => (
-        <Goal
-          key={uuidv4()}
-          title={goal.title}
-          goalId={goal._id}
-          actionPoints={goal.actionPoints}
-        />
+        <GoalContext.Provider
+        key={uuidv4()}
+        value={{ goalId: goal._id, actionPoints: goal.actionPoints }}
+        >
+          <Goal
+            title={goal.title}
+            goalId={goal._id}
+            actionPoints={goal.actionPoints}
+          />
+        </GoalContext.Provider>
       ))}
     </>
   );
