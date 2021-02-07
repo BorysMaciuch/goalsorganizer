2bbcba42ac6a86fbc8f648cb2db15ce8b291aa5e
BorysMaciuch <borysmaciuch@gmail.com>
2021-02-07 20:37:56 +0100

    Add Home page, SubHeading component, change Navbar styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Container } from "../../components/Container/styled";
+import { theme } from "../../styles";
+import { MainHeading, Paragraph } from "../../components/Typography/styled";
+import { StyledLink } from "../../components/NavBar/styled";
 
 const Home: React.FC = () => {
-    return (
-        <div>Homepage</div>
-    )
-}
+  return (
+    <Container bgColor={theme.colors.white}>
+      <MainHeading>Welcome to Goalsorganizer</MainHeading>
+      <Paragraph>
+        You can add your goals in{" "}
+        <StyledLink to="/goalslist">Goals List</StyledLink> section
+      </Paragraph>
+      <Paragraph>
+        After goal is added, add action points that needs to be adressed to
+        accomplish your goal
+      </Paragraph>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
