56a4e120e0319348022589f737395fdc81fc5992
BorysMaciuch <borysmaciuch@gmail.com>
2021-02-11 18:18:14 +0100

    Change routes

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ connection.once("open", () => {
   console.log("mongoDB database connection established successfully");
 });
 
-app.use("/goals", router);
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
